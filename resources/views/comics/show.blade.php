f00252c3440b5e9100c03412690c71b2ab13dfd0
MaryChiodelli <marychiodelli97@gmail.com>
2022-10-10 18:24:58 +0200

    add comic data

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -13,16 +13,16 @@
   <section class="comic-intro">
     <div class="container-sm">
       <div class="row">
-        <div class="col">
+        <div class="col-66">
           <h1>{{ $comic['title'] }}</h1>
-          <div>
-            <div>U.S. Price: {{ $comic['price'] }}</div>
+          <div class="list-inline price-info">
+            <div class="price">U.S. Price: {{ $comic['price'] }}</div>
             <div>available</div>
             <a class="btn">Check Availability +</a>
           </div>
           <p>{{ $comic['description'] }}</p>
         </div>
-        <div class="col">
+        <div class="col-33">
           <div>advertisement</div>
           <img src="{{ asset('img/adv.jpg') }}" alt="comic adv">
         </div>
@@ -31,11 +31,63 @@
   </section>
 
   <section class="comic-data">
-
+    <div class="container-sm">
+      <div class="row">
+        <div class="col-6">
+          <h2>Talent</h2>
+          <table>
+            <tr>
+              <th>Art by:</th>
+              <td>{{ implode(', ', $comic['artists']) }}</td>
+            </tr>
+            <tr>
+              <th>Art by:</th>
+              <td>{{ implode(', ', $comic['writers']) }}</td>
+            </tr>
+          </table>
+        </div>
+        <div class="col-6">
+          <h2>Specs</h2>
+          <table>
+            <tr>
+              <th>Series:</th>
+              <td>{{ $comic['series'] }}</td>
+            </tr>
+            <tr>
+              <th>U.S. Price:</th>
+              <td>{{ $comic['price'] }}</td>
+            </tr>
+            <tr>
+              <th>On Sale Date:</th>
+              <td>{{ $comic['sale_date'] }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
   </section>
 
-  <div class="inline-list">
-
+  <div class="comic-features">
+    <div class="container-sm features-wrapper">
+      <div class="row">
+        <div class="col-4">
+          <h3>Digital comics</h3>
+          <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+        </div>
+        <div class="col-4">
+          <h3>Shop DC</h3>
+          <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+        </div>
+        <div class="col-4">
+          <h3>Comic Shop Locator</h3>
+          <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+        </div>
+        <div class="col-4">
+          <h3>Subscriptions</h3>
+          <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 
 @endsection
