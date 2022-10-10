6c9db697a35c4533462f3e8ec43c5fb3a6b1097b
MaryChiodelli <marychiodelli97@gmail.com>
2022-10-10 17:21:32 +0200

    add hero + info section in comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,7 +3,39 @@
 @section('metaTitle', $comic['title'])
 
 @section('content')
-    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-    <h1>{{ $comic['title'] }}</h1>
-    <p>{{ $comic['description'] }}</p>
+
+  <div class="hero hero-comic">
+    <div class="container-sm">
+      <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+    </div>
+  </div>
+
+  <section class="comic-intro">
+    <div class="container-sm">
+      <div class="row">
+        <div class="col">
+          <h1>{{ $comic['title'] }}</h1>
+          <div>
+            <div>U.S. Price: {{ $comic['price'] }}</div>
+            <div>available</div>
+            <a class="btn">Check Availability +</a>
+          </div>
+          <p>{{ $comic['description'] }}</p>
+        </div>
+        <div class="col">
+          <div>advertisement</div>
+          <img src="{{ asset('img/adv.jpg') }}" alt="comic adv">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="comic-data">
+
+  </section>
+
+  <div class="inline-list">
+
+  </div>
+
 @endsection
